c60a4e6197185e4a34f185650c544af5d2b2a8ea
Insane96 <delvillano.alberto@gmail.com>
2025-05-15 10:33:21 +0200

    Lowered tiredness to effect

--- src/main/java/insane96mcp/iguanatweaksreborn/module/sleeprespawn/tiredness/Tiredness.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/module/sleeprespawn/tiredness/Tiredness.java
@@ -89,7 +89,7 @@ public class Tiredness extends JsonFeature {
 	public static Boolean shouldPreventSpawnPoint = false;
 	@Config(min = 0d)
 	@Label(name = "Tiredness for effect", description = "Tiredness required to get the Tired effect and be able to sleep.")
-	public static Double tirednessToEffect = 500d;
+	public static Double tirednessToEffect = 400d;
 	@Config(min = 0d)
 	@Label(name = "Tiredness per level", description = "Every this Tiredness above 'Tiredness for effect' will add a new level of Tired.")
 	public static Double tirednessPerLevel = 250d;
