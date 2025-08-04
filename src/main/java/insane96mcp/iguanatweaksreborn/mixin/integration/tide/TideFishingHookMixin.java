5a167964b7f897f68f7128ed539fd32e51e1658d
Insane96 <delvillano.alberto@gmail.com>
2025-08-04 12:45:47 +0200

    4.19.1 - Startup crash with Tide

--- src/main/java/insane96mcp/iguanatweaksreborn/mixin/integration/tide/TideFishingHookMixin.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/mixin/integration/tide/TideFishingHookMixin.java
@@ -10,19 +10,16 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(TideFishingHook.class)
 public abstract class TideFishingHookMixin extends Projectile {
-    @Shadow private int timeUntilHooked;
-
     protected TideFishingHookMixin(EntityType<? extends Projectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     @Definition(id = "nextInt", method = "Lnet/minecraft/util/Mth;nextInt(Lnet/minecraft/util/RandomSource;II)I")
-    @Definition(id = "random", field = "Lcom/li64/tide/registries/entities/misc/fishing/TideFishingHook;random:Lnet/minecraft/util/RandomSource;")
+    @Definition(id = "random", field = "Lcom/li64/tide/registries/entities/misc/fishing/TideFishingHook;random:Lnet/minecraft/util/RandomSource;", remap = false)
     @Expression("nextInt(this.random, 20, 80)")
     @ModifyExpressionValue(method = "catchingFish", at = @At(value = "MIXINEXTRAS:EXPRESSION"), remap = false)
     public int iguanatweaksreborn$hookingFishEvent(int original) {
@@ -30,7 +27,7 @@ public abstract class TideFishingHookMixin extends Projectile {
     }
 
     @Definition(id = "nextInt", method = "Lnet/minecraft/util/Mth;nextInt(Lnet/minecraft/util/RandomSource;II)I")
-    @Definition(id = "random", field = "Lcom/li64/tide/registries/entities/misc/fishing/TideFishingHook;random:Lnet/minecraft/util/RandomSource;")
+    @Definition(id = "random", field = "Lcom/li64/tide/registries/entities/misc/fishing/TideFishingHook;random:Lnet/minecraft/util/RandomSource;", remap = false)
     @Expression("nextInt(this.random, 200, 600)")
     @ModifyExpressionValue(method = "catchingFish", at = @At(value = "MIXINEXTRAS:EXPRESSION"), remap = false)
     public int iguanatweaksreborn$luringFishEvent(int original) {
