64c41fbdacdb8103aaa4b3c2b1e0946d96c027f1
Insane96 <delvillano.alberto@gmail.com>
2026-03-02 18:22:44 +0100

    Finished porting Misc Client

---
+++ src/main/java/insane96mcp/insanesurvivaloverhaul/mixin/client/GuiMixin.java
@@ -0,0 +1,146 @@
+package insane96mcp.insanesurvivaloverhaul.mixin.client;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import insane96mcp.insanesurvivaloverhaul.module.client.Misc;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PlayerRideableJumping;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Gui.class)
+public abstract class GuiMixin {
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+
+    @Shadow
+    protected abstract boolean isExperienceBarVisible();
+
+    @Shadow
+    protected abstract void renderExperienceBar(GuiGraphics guiGraphics, int x);
+
+    @ModifyExpressionValue(method = "renderItemHotbar", at = @At(value = "CONSTANT", args = "intValue=22", ordinal = 0))
+    public int hotBarShift(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderItemHotbar", at = @At(value = "CONSTANT", args = "intValue=22", ordinal = 2))
+    public int selectedHotBarShift(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderItemHotbar", at = @At(value = "CONSTANT", args = "intValue=23", ordinal = 1))
+    public int offHandShiftLeft(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderItemHotbar", at = @At(value = "CONSTANT", args = "intValue=23", ordinal = 2))
+    public int offHandShiftRight(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderItemHotbar", at = @At(value = "CONSTANT", args = "intValue=16", ordinal = 0))
+    public int renderItems(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderItemHotbar", at = @At(value = "CONSTANT", args = "intValue=16", ordinal = 1))
+    public int renderItemsOffhand(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderItemHotbar", at = @At(value = "CONSTANT", args = "intValue=20", ordinal = 2))
+    public int renderAttackIndicator(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderExperienceBar", at = @At(value = "CONSTANT", args = "intValue=32", ordinal = 0))
+    public int experienceBarAndLevelShift(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyExpressionValue(method = "renderJumpMeter", at = @At(value = "CONSTANT", args = "intValue=32", ordinal = 0))
+    public int jumpMeterShift(int original) {
+        if (!Misc.shouldRaiseHotbar())
+            return original;
+        return original + Misc.floatyHotbar;
+    }
+
+    @ModifyVariable(method = "renderVehicleHealth", at = @At(value = "STORE", ordinal = 0), ordinal = 1)
+    public int iguanatweaksreborn$onGetVehicleHealth(int original, @Local LivingEntity mount) {
+        if (!Misc.fixMountsGui())
+            return original;
+        return (int)(mount.getMaxHealth() + 0.5F) / 2;
+    }
+
+    @Definition(id = "l2", local = @Local(type = int.class))
+    @Expression("l2 == 0")
+    @WrapOperation(method = "renderFoodLevel", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+    public boolean iguanatweaksreborn$onTryRenderFood(int left, int right, Operation<Boolean> original, @Local LivingEntity mount) {
+        if (!Misc.fixMountsGui())
+            return original.call(left, right);
+        return true;
+    }
+
+    @Definition(id = "playerrideablejumping", local = @Local(type = PlayerRideableJumping.class))
+    @Expression("playerrideablejumping != null")
+    @WrapOperation(method = "maybeRenderJumpMeter", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+    public boolean iguanatweaksreborn$onGetVehicleHealth(Object left, Object right, Operation<Boolean> original) {
+        if (!Misc.fixMountsGui())
+            return original.call(left, right);
+        //noinspection DataFlowIssue
+        return original.call(left, right) && (minecraft.player.getJumpRidingScale() > 0/* || DroppedExperience.disableExperience*/);
+    }
+
+    @Inject(method = "maybeRenderExperienceBar", at = @At(value = "HEAD"), cancellable = true)
+    public void iguanatweaksreborn$onTryRenderXpBar(GuiGraphics guiGraphics, DeltaTracker p_348543_, CallbackInfo ci) {
+        if (!Misc.fixMountsGui())
+            return;
+        ci.cancel();
+        int i = guiGraphics.guiWidth() / 2 - 91;
+        if ((this.minecraft.player.jumpableVehicle() == null && this.isExperienceBarVisible()) || minecraft.player.getJumpRidingScale() == 0) {
+            this.renderExperienceBar(guiGraphics, i);
+        }
+    }
+
+    @Definition(id = "pCurrentHealth", local = @Local(type = int.class, ordinal = 4, argsOnly = true))
+    @Definition(id = "pAbsorptionAmount", local = @Local(type = int.class, ordinal = 6, argsOnly = true))
+    @Expression("pCurrentHealth + pAbsorptionAmount <= 4")
+    @ModifyExpressionValue(method = "renderHearts", at = @At("MIXINEXTRAS:EXPRESSION"))
+    public boolean iguanatweaksreborn$onShakeCheck(boolean original, @Local(argsOnly = true) float pMaxHealth) {
+        if (!Misc.shouldPreventHealthShake()
+                || !original)
+            return original;
+        return pMaxHealth > 4;
+    }
+}
--- src/main/java/insane96mcp/insanesurvivaloverhaul/module/client/Misc.java
+++ src/main/java/insane96mcp/insanesurvivaloverhaul/module/client/Misc.java
@@ -4,6 +4,12 @@ import insane96mcp.insanelib.core.feature.Feature;
 import insane96mcp.insanelib.core.feature.LoadFeature;
 import insane96mcp.insanelib.core.feature.config.Config;
 import insane96mcp.insanesurvivaloverhaul.module.ISOClientModules;
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
 
 @LoadFeature(module = ISOClientModules.CLIENT)
 public class Misc extends Feature {
@@ -13,21 +19,21 @@ public class Misc extends Feature {
     @Config(description = "If true, the outline around blocks will be red if the tool in hand will make drops not ... drop.")
     public static Boolean redBlockOutlineWithWrongTool = true;
 
-    //@Config(min = 0, description = "Moves the hotbar this amount of pixels up (like bedrock edition). Other mods' GUI elements should work flawlessly if using the correct Forge GUI fields")
-    //public static Integer floatyHotbar = 2;
-    //@Config(description = "If true, hunger will be rendered even when the player is in riding an entity. Also hides the jump bar unless jumping (showing the XP bar)")
-    //public static Boolean fixMountsGui = true;
+    @Config(min = 0, description = "Moves the hotbar this amount of pixels up (like bedrock edition). Other mods' GUI elements should work flawlessly if using the correct Forge GUI fields")
+    public static Integer floatyHotbar = 2;
+    @Config(description = "If true, hunger will be rendered even when the player is in riding an entity. Also hides the jump bar unless jumping (showing the XP bar)")
+    public static Boolean fixMountsGui = true;
 
-    //@Config(description = "If true, health will not shake if player's max health is below 4")
-    //public static Boolean preventShakeBasedOffMaxHealth = true;
+    @Config(description = "If true, health will not shake if player's max health is below 4")
+    public static Boolean preventShakeBasedOffMaxHealth = true;
 
     public static boolean shouldDisableTiltingWithNonDirectionalDamageTypes() {
         return isEnabled(Misc.class) && noTiltingWithNonDirectionalDamageTypes;
     }
 
-    /*public static boolean fixMountsGui() {
+    public static boolean fixMountsGui() {
         return isEnabled(Misc.class) && fixMountsGui;
-    }*/
+    }
 
     public static float getRedOutlineAmount(float original) {
         if (!isEnabled(Misc.class) || !redBlockOutlineWithWrongTool)
@@ -36,16 +42,14 @@ public class Misc extends Feature {
     }
 
     //Render before Regenerating absorption
-    /*@OnlyIn(Dist.CLIENT)
+    @OnlyIn(Dist.CLIENT)
     @SubscribeEvent(priority = EventPriority.HIGH)
-    public void riseLeftAndRightHeight(final RenderGuiOverlayEvent.Pre event) {
+    public void riseLeftAndRightHeight(final RenderGuiEvent.Pre event) {
         if (!shouldRaiseHotbar())
             return;
 
-        if (event.getOverlay().equals(VanillaGuiOverlay.VIGNETTE.type())) {
-            ((ForgeGui) Minecraft.getInstance().gui).rightHeight += floatyHotbar;
-            ((ForgeGui) Minecraft.getInstance().gui).leftHeight += floatyHotbar;
-        }
+        Minecraft.getInstance().gui.leftHeight += floatyHotbar;
+        Minecraft.getInstance().gui.rightHeight += floatyHotbar;
     }
 
     public static boolean shouldRaiseHotbar() {
@@ -54,5 +58,5 @@ public class Misc extends Feature {
 
     public static boolean shouldPreventHealthShake() {
         return Feature.isEnabled(Misc.class) && preventShakeBasedOffMaxHealth;
-    }*/
+    }
 }
