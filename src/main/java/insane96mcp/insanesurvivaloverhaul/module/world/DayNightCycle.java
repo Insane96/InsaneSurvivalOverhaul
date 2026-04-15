07f6eaeb485d5dc32f50522e29de1012715924ac
Alberto Del Villano <delvillano.alberto@gmail.com>
2026-04-15 10:35:25 +0200

    minor config description change

--- src/main/java/insane96mcp/insanesurvivaloverhaul/module/world/DayNightCycle.java
+++ src/main/java/insane96mcp/insanesurvivaloverhaul/module/world/DayNightCycle.java
@@ -7,7 +7,7 @@ import insane96mcp.insanesurvivaloverhaul.module.ISOModules;
 
 @LoadFeature(module = ISOModules.WORLD, canBeDisabled = false)
 public class DayNightCycle extends Feature {
-    @Config(description = "How many minutes does a full day last? This DOESN'T CHANGE the day night cycle duration, but it's only used for calculations in case another mod changes it.")
+    @Config(description = "How many minutes does a full day last? This DOESN'T CHANGE the day night cycle duration, but it's only used for calculations in case you change it with another mod.")
     public static Double dayNightCycleDuration = 20d;
 
     /**
