23e6689bfce31571eada7fc68ac3c18b69e5d680
Insane96 <delvillano.alberto@gmail.com>
2026-04-16 18:47:26 +0200

    Re-added Armor Rework formula

--- src/main/java/insane96mcp/insanesurvivaloverhaul/module/combat/ArmorRework.java
+++ src/main/java/insane96mcp/insanesurvivaloverhaul/module/combat/ArmorRework.java
@@ -1,16 +1,37 @@
 package insane96mcp.insanesurvivaloverhaul.module.combat;
 
+import com.ezylang.evalex.Expression;
+import com.ezylang.evalex.data.EvaluationValue;
 import insane96mcp.insanelib.core.feature.Feature;
 import insane96mcp.insanelib.core.feature.LoadFeature;
+import insane96mcp.insanelib.core.feature.config.Config;
+import insane96mcp.insanesurvivaloverhaul.InsaneSO;
 import insane96mcp.insanesurvivaloverhaul.module.ISOModules;
 
 @LoadFeature(module = ISOModules.COMBAT, description = "Change armor formula to damage * (1 - 1.5 * (armor / (armor + 20)))")
 public class ArmorRework extends Feature {
 
-    //@Config(description = "Vanilla formula is 'damage * (1 - ((MIN(20, MAX(armor / 5, armor - ((4 * damage) / (toughness + 8)))))) / 25))'")
-    //public static String formula = "damage * (1 - 1.5 * (armor / (armor + 20)))";
+    @Config(description = "Vanilla formula is 'damage * (1 - ((MIN(20, MAX(armor / 5, armor - ((4 * damage) / (toughness + 8)))))) / 25))'")
+    public static String formula = "damage * (1 - 1.5 * (armor / (armor + 20)))";
+
+    //public static float getCalculatedDamage(float damage, float armor, float toughness) {
+    //    return damage * (1 - 1.5f * (armor / (armor + 20)));
+    //}
 
     public static float getCalculatedDamage(float damage, float armor, float toughness) {
-        return damage * (1 - 1.5f * (armor / (armor + 20)));
+        Expression expression = new Expression(formula);
+        try {
+            //noinspection ConstantConditions
+            EvaluationValue result = expression
+                    .with("damage", damage)
+                    .and("armor", armor)
+                    .and("toughness", toughness)
+                    .evaluate();
+            return result.getNumberValue().floatValue();
+        }
+        catch (Exception ex) {
+            InsaneSO.LOGGER.error("Failed to evaluate armor formula: {}\n{}", formula, ex);
+            return -1f;
+        }
     }
 }
