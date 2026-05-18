abdc6679e7e6d36eedb833c8bc51d6d77df4fdcd
Insane96 <delvillano.alberto@gmail.com>
2026-05-18 20:01:06 +0200

    Added copper_furnace integrated data pack

--- src/main/java/insane96mcp/insanesurvivaloverhaul/module/misc/Packs.java
+++ src/main/java/insane96mcp/insanesurvivaloverhaul/module/misc/Packs.java
@@ -15,10 +15,13 @@ public class Packs extends Feature {
 
     @Config(description = "If true a data pack will be enabled that disables villages and pillagers outpost generation.")
     public static Boolean disableLongNosesStructures = true;
+    @Config(description = "If true a data pack will be enabled that makes furnaces require copper. Copper ingots can be obtained from raw copper on campfires.")
+    public static Boolean copperFurnace = true;
 
     @Override
     public void init(Module module, boolean enabledByDefault, boolean canBeDisabled) {
         super.init(module, enabledByDefault, canBeDisabled);
         InsaneSO.addServerPack("disable_long_noses", "Insane's Survival Overhaul Disable Long Noses", () -> this.isEnabled() && !Packs.disableAllDataPacks && disableLongNosesStructures);
+        InsaneSO.addServerPack("copper_furnace", "Insane's Survival Overhaul Copper Furnace", () -> this.isEnabled() && !Packs.disableAllDataPacks && copperFurnace);
     }
 }
