e061ee9db7c016d0b479c393e17f662c927f838b
Alberto Del Villano <delvillano.alberto@gmail.com>
2026-06-28 09:33:46 +0200

    Added rune enchanting integration

--- src/main/java/insane96mcp/insanesurvivaloverhaul/module/mining/anvilcrafting/RuneEnchantingIntegration.java
+++ src/main/java/insane96mcp/insanesurvivaloverhaul/module/mining/anvilcrafting/RuneEnchantingIntegration.java
@@ -1,5 +1,6 @@
 package insane96mcp.insanesurvivaloverhaul.module.mining.anvilcrafting;
 
+import insane96mcp.runeenchanting.RuneHelper;
 import net.minecraft.world.item.ItemStack;
 
 public class RuneEnchantingIntegration {
